6899f56476292f615197d5d2828a76f11928b34f
yona-hammouchi <yona.hammouchi@laplateforme.io>
2025-01-22 13:06:13 +0100

    init day 3

---
+++ jour03/job02/index.php
@@ -0,0 +1,8 @@
+<?php
+
+$str = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie.";
+
+// Parcours de la chaîne en affichant un caractère sur deux
+for ($i = 0; $i < strlen($str); $i += 2) {
+    echo $str[$i];
+}
