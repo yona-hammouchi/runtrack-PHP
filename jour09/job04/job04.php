98a2aca99ee5415fda46c6bf852c071aed873c8a
yona-hammouchi <yona.hammouchi@laplateforme.io>
2025-01-25 21:29:02 +0100

    Ajout : Récupération et affichage des données étudiantes dans des tableaux HTML - Récupération des étudiantes avec prénom, nom et date de naissance. - Récupération des étudiants avec un prénom commençant par 'T'. - Récupération des étudiants de moins de 18 ans. - Comptage du nombre total d'étudiants et affichage dans une colonne 'nb_etudiants'.

---
+++ jour09/job04/job04.php
@@ -0,0 +1,41 @@
+<?php
+
+$jour08 = new PDO(
+    'mysql:host=localhost;dbname=jour08;charset=utf8',
+    'root',
+    'root'
+);
+
+//prepare et exécute la requête SQL
+$query = $jour08->prepare("SELECT * FROM etudiants WHERE prenom LIKE 'T%'");
+$query->execute();
+
+// Récupérer toutes les données
+$etudiants = $query->fetchAll(PDO::FETCH_ASSOC);
+
+// Vérifier si des données existent
+if (count($etudiants) > 0) {
+    echo '<table border="1">';
+
+    // Afficher le <thead> avec les noms des colonnes
+    echo '<thead><tr>';
+    foreach (array_keys($etudiants[0]) as $columnName) {
+        echo '<th>' . htmlspecialchars($columnName) . '</th>';
+    }
+    echo '</tr></thead>';
+
+    // Afficher le <tbody> avec les données
+    echo '<tbody>';
+    foreach ($etudiants as $etudiants) {
+        echo '<tr>';
+        foreach ($etudiants as $value) {
+            echo '<td>' . htmlspecialchars($value) . '</td>';
+        }
+        echo '</tr>';
+    }
+    echo '</tbody>';
+
+    echo '</table>';
+} else {
+    echo 'Aucune donnée trouvée dans la table "etudiants".';
+}
